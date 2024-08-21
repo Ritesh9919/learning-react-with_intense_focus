256cc866549004f2d8a85baf0632af7995f3e99e
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-08-21 10:34:32 +0530

    learned what and why react and how to create element in react using CND

---
+++ 01_react-intro/index.js
@@ -0,0 +1,18 @@
+// using javascript
+// const h1 = document.createElement('h1');
+// h1.className = "heading";
+// h1.textContent = "Hello World";
+// const div = document.getElementById("root");
+// div.append(h1);
+// document.body.append(div);
+// console.log(h1);
+
+
+// using react
+// const reactHeading = React.createElement('h1', {className:"heading", children:"Hello World"});
+// console.log(reactHeading);
+// ReactDOM.createRoot(document.getElementById('root')).render(reactHeading);
+
+const img = React.createElement('img', {src:"https://files.codingninjas.in/coding-ninjas-24647.png"});
+ReactDOM.createRoot(document.getElementById("root")).render(img);
+
