70c5e2bd098b955faef5f9cc7289a937d9923716
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-08-21 13:18:31 +0530

    rendering array and object in jsx

--- 02_jsx/index.js
+++ 02_jsx/index.js
@@ -13,6 +13,10 @@ function App() {
 
     const header = <h2>JSX Expression</h2>
 
+    // array and object
+    const numbers = [1, 2, 3, 4, 5];
+    const user = {name:"Ritesh", age:25}
+
     return (
         <>
         <h1>Let's learn JSX!</h1>
@@ -23,7 +27,13 @@ function App() {
         <p>Undefined Value: {demo2}</p>
         <p>Boolean Value: {demo3}</p>
         <p>Calling a function: {sum()}</p>
+        {/* rendering array */}
+        {numbers.map((num)=> 
+         <h2>{num}</h2>
+        )}
 
+         {/* rendering object */}
+         <h3>My name is: {user.name} and my age is: {user.age}</h3>
         </>
     )
 }
