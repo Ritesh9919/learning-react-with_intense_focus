02f9592351206d1c7f25fe05421e07a13fed2df3
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-08-22 14:10:43 +0530

    added some conditional to app

--- 03_score-kepper-app/index.js
+++ 03_score-kepper-app/index.js
@@ -1,35 +1,41 @@
 let score = 0;
 let wicket = 0;
 
-const App = ()=> {
-    
-
-    function addScore(num){
-        score+=num;
-        rootElement.render(<App/>);
-        
-        
+const App = () => {
+  function addScore(num) {
+    if (wicket < 10) {
+      score += num;
+      rootElement.render(<App />);
     }
+  }
 
-    
+  function addWicket() {
+    if (wicket < 10) {
+      wicket += 1;
+      rootElement.render(<App />);
+    }
+  }
 
-    return (
-        <>
-        <h1>SCORE KEEPER</h1>
-        <h2>SCORE : {score}/{wicket}</h2>
-        <div>
-            <button onClick={()=> addScore(1)}>1</button>
-            <button onClick={()=> addScore(2)}>2</button>
-            <button onClick={()=> addScore(3)}>3</button>
-            <button onClick={()=> addScore(4)}>4</button>
-            <button onClick={()=> addScore(5)}>5</button>
-            <button onClick={()=> addScore(6)}>6</button>
-            <button className="wicket">Wicket</button>
-        </div>
-        </>
-    )
-}
-
+  return (
+    <>
+      <h1>SCORE KEEPER</h1>
+      <h2>
+        SCORE : {score}/{wicket}
+      </h2>
+      <div>
+        <button onClick={() => addScore(1)}>1</button>
+        <button onClick={() => addScore(2)}>2</button>
+        <button onClick={() => addScore(3)}>3</button>
+        <button onClick={() => addScore(4)}>4</button>
+        <button onClick={() => addScore(5)}>5</button>
+        <button onClick={() => addScore(6)}>6</button>
+        <button className="wicket" onClick={addWicket}>
+          Wicket
+        </button>
+      </div>
+    </>
+  );
+};
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(<App/>);
+rootElement.render(<App />);
