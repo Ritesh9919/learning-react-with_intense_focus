b939a9acc4293e9a296ca370755cd3c9d294836b
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-08-22 14:26:09 +0530

    created scoreButtons component and stored score in array

--- 03_score-kepper-app/index.js
+++ 03_score-kepper-app/index.js
@@ -1,38 +1,49 @@
 let score = 0;
 let wicket = 0;
+const ballWiseResult = [];
+
+function addScore(num) {
+  if (wicket < 10) {
+    ballWiseResult.push(num);
+    score += num;
+    rootElement.render(<App />);
+    console.log(ballWiseResult);
+  }
+}
+
+function addWicket() {
+  if (wicket < 10) {
+    ballWiseResult.push("W");
+    wicket += 1;
+    rootElement.render(<App />);
+  }
+}
+
+const ScoreButtons = () => {
+  return (
+    <div>
+      <button onClick={() => addScore(0)}>0</button>
+      <button onClick={() => addScore(1)}>1</button>
+      <button onClick={() => addScore(2)}>2</button>
+      <button onClick={() => addScore(3)}>3</button>
+      <button onClick={() => addScore(4)}>4</button>
+      <button onClick={() => addScore(5)}>5</button>
+      <button onClick={() => addScore(6)}>6</button>
+      <button className="wicket" onClick={addWicket}>
+        Wicket
+      </button>
+    </div>
+  );
+};
 
 const App = () => {
-  function addScore(num) {
-    if (wicket < 10) {
-      score += num;
-      rootElement.render(<App />);
-    }
-  }
-
-  function addWicket() {
-    if (wicket < 10) {
-      wicket += 1;
-      rootElement.render(<App />);
-    }
-  }
-
   return (
     <>
       <h1>SCORE KEEPER</h1>
       <h2>
         SCORE : {score}/{wicket}
       </h2>
-      <div>
-        <button onClick={() => addScore(1)}>1</button>
-        <button onClick={() => addScore(2)}>2</button>
-        <button onClick={() => addScore(3)}>3</button>
-        <button onClick={() => addScore(4)}>4</button>
-        <button onClick={() => addScore(5)}>5</button>
-        <button onClick={() => addScore(6)}>6</button>
-        <button className="wicket" onClick={addWicket}>
-          Wicket
-        </button>
-      </div>
+      <ScoreButtons addScore={addScore} addWicket={addWicket} />
     </>
   );
 };
