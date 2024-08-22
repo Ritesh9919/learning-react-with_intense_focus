de713bf4f6242dd431bfd769ff042c34f0223852
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-08-22 13:53:43 +0530

    more on event handling

--- 03_score-kepper-app/index.js
+++ 03_score-kepper-app/index.js
@@ -1,24 +1,29 @@
-
+let score = 0;
+let wicket = 0;
 
 const App = ()=> {
-    let score = 0;
-    let wicket = 0;
+    
 
-    const clickOne = ()=> {
-        alert("1 is clicket");
+    function addScore(num){
+        score+=num;
+        rootElement.render(<App/>);
+        
+        
     }
+
+    
 
     return (
         <>
         <h1>SCORE KEEPER</h1>
         <h2>SCORE : {score}/{wicket}</h2>
         <div>
-            <button onClick={clickOne}>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
+            <button onClick={()=> addScore(1)}>1</button>
+            <button onClick={()=> addScore(2)}>2</button>
+            <button onClick={()=> addScore(3)}>3</button>
+            <button onClick={()=> addScore(4)}>4</button>
+            <button onClick={()=> addScore(5)}>5</button>
+            <button onClick={()=> addScore(6)}>6</button>
             <button className="wicket">Wicket</button>
         </div>
         </>
@@ -26,4 +31,5 @@ const App = ()=> {
 }
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(<App/>);
